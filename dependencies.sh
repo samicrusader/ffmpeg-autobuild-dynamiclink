99a68133962a23672b052c35a1daeba4b702cfaf
samicrusader <hi@samicrusader.me>
2023-05-17 17:29:29 -0400

    this will still error but should push right

--- dependencies.sh
+++ dependencies.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 sudo apt-get -qq update
 sudo apt-get -qq upgrade
-sudo apt-get -qq install ragel cvs yasm pax nasm gperf autogen autoconf-archive
+sudo apt-get -qq install ragel cvs yasm pax nasm gperf autogen autoconf-archive p7zip-full
 sudo -H pip3 -qq install meson ninja
